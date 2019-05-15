dc909512b39acada7e85b42586516393fdee5ac7
Kyle Sprague <boilscene@gmail.com>
2019-05-15 11:45:59 -0400

    Update __init__.py

--- Games/__init__.py
+++ Games/__init__.py
@@ -3,7 +3,19 @@ from gym.envs.registration import register
 #woot
 register(
     id='Tetris-v0',
-    entry_point='Games.Tetris:TetrisEnv',
-    max_episode_steps = 1000,
+    entry_point='Games.Tetris.environment:TetrisEnv',
 )
 
+register(
+    id='Tetris-v1',
+    entry_point='Games.Tetrisv1.environment:TetrisEnv',
+)
+register(
+    id='Dominos-v0',
+    entry_point='Games.Dominos.environment:TetrisEnv',
+)
+
+register(
+    id='Triminos-v0',
+    entry_point='Games.Triminos.environment:TetrisEnv',
+)
